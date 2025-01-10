7acfd72e9031f412971d2f8bf073e1e029029035
omkar6522 <omkarpatil6522@gmail.com>
2025-01-10 15:55:58 +0530

    My 3nd Commit

---
+++ src/Git_Push_Commands.java
@@ -0,0 +1,10 @@
+public class Git_Push_Commands {
+    // git init
+    // git commit -m "first commit"
+    // git add .
+    // git status
+    // git branch -M main
+    //git remote add origin https://github.com/omkar6522/Omkar_Java_Automation.git
+    // git push -u origin main
+    // show error failed to push some refs to  'https://github.com/omkar6522/Omkar_Java_Automation.git'
+}
